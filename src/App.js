14e3cb7f17e1fad43e67d13e54a22aa8684cf20b
dheeraj <dheerajkarwasra42@gmail.com>
2021-06-23 08:07:53 -0700

    styled the entire app

--- src/App.js
+++ src/App.js
@@ -49,17 +49,25 @@ const App = () => {
 
   return (
     <div className="app">
-      <h1>TIC TAC TOE</h1>
+      <h1>
+        TIC <span className="text-green">TAC</span> TOE
+      </h1>
       <StatusMessage winner={winner} current={current} />
       <Board
         board={current.board}
         handleSquareClick={handleSquareClick}
         winningSquares={winningSquares}
       />
-      <bitton type="button" onClick={onNewGame}>
+      <bitton
+        type="button"
+        onClick={onNewGame}
+        className={`btn-reset ${winner ? 'active' : ''}`}
+      >
         Start New Game
       </bitton>
+      <h2 style={{ fontWeight: 'normal' }}>Current game history</h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
+      <div className="bg-balls" />
     </div>
   );
 };
--- src/components/StatusMessage.js
+++ src/components/StatusMessage.js
@@ -8,13 +8,30 @@ const StatusMessage = ({ winner, current }) => {
   const noMovesLeft = current.board.every(el => el !== null);
 
   return (
-    <h2>
-      {winner && `WINNER IS ${winner}`}
-      {!winner &&
-        !noMovesLeft &&
-        `NEXT PLAYER IS ${current.isXNext ? 'X' : 'O'}`}
-      {!winner && noMovesLeft && 'X and O tied'}
-    </h2>
+    <div className="status-message">
+      {winner && (
+        <>
+          winner is{' '}
+          <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
+            {winner}
+          </span>
+        </>
+      )}
+      {!winner && !noMovesLeft && (
+        <>
+          NEXT PLAYER IS{' '}
+          <span className={current.isXNext ? 'text-green' : 'text-orange'}>
+            {current.isXNext ? 'X' : 'O'}
+          </span>
+        </>
+      )}
+      {!winner && noMovesLeft && (
+        <>
+          <span className="text-green">X</span>and
+          <span className="text-orange">O</span>tied
+        </>
+      )}
+    </div>
   );
 };
 
